5076363890d93fe14db8047bc3bc293dd87ef2fa
Nikita <strukalinnikita07@gmail.com>
2025-11-15 16:09:28 +0300

    Add files via upload
    Start developing

---
+++ Nexora iOS/Nexora iOS/Item.swift
@@ -0,0 +1,7 @@
+//
+//  Item.swift
+//  Nexora iOS
+//
+//  Created by Никита Струкалин on 15.11.2025.
+//
+
---
+++ Nexora iOS/Nexora iOS/Nexora_iOSApp.swift
@@ -0,0 +1,18 @@
+//
+//  Nexora_iOSApp.swift
+//  Nexora iOS
+//
+//  Created by Никита Струкалин on 15.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Nexora_iOSApp: App {
+    var body: some Scene {
+        WindowGroup() {
+            ContentView()
+        }
+    }
+}
+
